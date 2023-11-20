9cad75ead6866d49da1a50bd84ebedeb5748903e
NecinChikito <125698303+NecinChikito@users.noreply.github.com>
2023-11-20 15:30:00 -0500

    Add files via upload

---
+++ TorreHanoi/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace TorreHanoi
+{
+    partial class Hanoi
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.comboInicio = new System.Windows.Forms.ComboBox();
+            this.comboFinal = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnResolver = new System.Windows.Forms.Button();
+            this.comboNumDiscos = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.BackColor = System.Drawing.Color.Transparent;
+            this.btnInicio.BackgroundImage = global::TorreHanoi.Properties.Resources.image_processing20210627_9645_gggjz1;
+            this.btnInicio.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnInicio.FlatAppearance.BorderSize = 0;
+            this.btnInicio.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnInicio.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInicio.Font = new System.Drawing.Font("Unispace", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicio.Location = new System.Drawing.Point(12, 12);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(129, 104);
+            this.btnInicio.TabIndex = 1;
+            this.btnInicio.Text = "Iniciar";
+            this.btnInicio.UseVisualStyleBackColor = false;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // comboInicio
+            // 
+            this.comboInicio.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.comboInicio.FormattingEnabled = true;
+            this.comboInicio.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.comboInicio.Location = new System.Drawing.Point(147, 58);
+            this.comboInicio.Name = "comboInicio";
+            this.comboInicio.Size = new System.Drawing.Size(121, 21);
+            this.comboInicio.TabIndex = 3;
+            this.comboInicio.Text = "Torre inicial";
+            this.comboInicio.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboFinal
+            // 
+            this.comboFinal.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.comboFinal.FormattingEnabled = true;
+            this.comboFinal.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.comboFinal.Location = new System.Drawing.Point(147, 95);
+            this.comboFinal.Name = "comboFinal";
+            this.comboFinal.Size = new System.Drawing.Size(121, 21);
+            this.comboFinal.TabIndex = 4;
+            this.comboFinal.Text = "Torre final";
+            this.comboFinal.SelectedIndexChanged += new System.EventHandler(this.comboFinal_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SandyBrown;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 122);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(820, 316);
+            this.panel1.TabIndex = 5;
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Transparent;
+            this.btnReset.BackgroundImage = global::TorreHanoi.Properties.Resources.image_processing20210627_9645_r5owm1;
+            this.btnReset.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReset.FlatAppearance.BorderColor = System.Drawing.Color.LightSalmon;
+            this.btnReset.FlatAppearance.BorderSize = 0;
+            this.btnReset.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnReset.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Unispace", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(566, 6);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(266, 110);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reiniciar";
+            this.btnReset.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnResolver
+            // 
+            this.btnResolver.BackColor = System.Drawing.Color.Transparent;
+            this.btnResolver.BackgroundImage = global::TorreHanoi.Properties.Resources.image_processing20210627_9645_1xi0g58;
+            this.btnResolver.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnResolver.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnResolver.FlatAppearance.BorderSize = 0;
+            this.btnResolver.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnResolver.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnResolver.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResolver.Font = new System.Drawing.Font("Unispace", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResolver.Location = new System.Drawing.Point(316, 12);
+            this.btnResolver.Name = "btnResolver";
+            this.btnResolver.Size = new System.Drawing.Size(244, 104);
+            this.btnResolver.TabIndex = 0;
+            this.btnResolver.Text = "Resolver";
+            this.btnResolver.UseVisualStyleBackColor = false;
+            this.btnResolver.Click += new System.EventHandler(this.btnResolver_Click_1);
+            // 
+            // comboNumDiscos
+            // 
+            this.comboNumDiscos.FormattingEnabled = true;
+            this.comboNumDiscos.Items.AddRange(new object[] {
+            "3",
+            "4",
+            "5"});
+            this.comboNumDiscos.Location = new System.Drawing.Point(147, 22);
+            this.comboNumDiscos.Name = "comboNumDiscos";
+            this.comboNumDiscos.Size = new System.Drawing.Size(121, 21);
+            this.comboNumDiscos.TabIndex = 6;
+            this.comboNumDiscos.Text = "Número de discos";
+            this.comboNumDiscos.SelectedIndexChanged += new System.EventHandler(this.comboNumDiscos_SelectedIndexChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.SaddleBrown;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(0, 226);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(820, 90);
+            this.panel2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(276, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Terminado!";
+            this.label1.Visible = false;
+            // 
+            // Hanoi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.BackgroundImage = global::TorreHanoi.Properties.Resources.b9c8f893c9a782033a01f47e0c0b1d6e;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(844, 450);
+            this.Controls.Add(this.comboNumDiscos);
+            this.Controls.Add(this.comboFinal);
+            this.Controls.Add(this.comboInicio);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.btnResolver);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Hanoi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Torre de hanoi";
+            this.Load += new System.EventHandler(this.Hanoi_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnResolver;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.ComboBox comboInicio;
+        private System.Windows.Forms.ComboBox comboFinal;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboNumDiscos;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TorreHanoi/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TorreHanoi
+{
+    public partial class Hanoi : Form
+    {
+        private int numDiscos ;
+        private int TorreInicial;
+        private int TorreAuxiliar;
+        private int TorreFinal;
+        private List<Panel> torresVisuales;
+        private List<Stack<int>> torres;
+        private List<Button> discos;
+        private string inicioText = "Torre inicial";
+        private string finalText = "Torre final";
+        
+        public Hanoi()
+        {InitializeComponent();IniciarTorresVisuales();MostrarTorresVisuales();}
+        
+        private void IniciarTorres()
+        {
+            torres = new List<Stack<int>>();
+            for (int i = 0; i < 3 ; i++)
+            {
+                torres.Add(new Stack<int>());
+            }
+            for (int i = numDiscos; i > 0 ; i--)
+            {
+                torres[TorreInicial].Push(i);
+            }
+        }
+        private void IniciarDiscos()
+        {
+            Random rand = new Random();
+            discos = new List<Button>(); 
+            for (int i = 0; i < numDiscos; i++)
+            {
+                Button disco = new Button();             
+                disco.Width = (i + 1) * 30 + 8;
+                disco.FlatStyle = FlatStyle.Flat; 
+                Color randomColor = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));                
+                disco.BackColor = randomColor; 
+                discos.Add(disco);
+            }
+        }
+        private void MostrarDiscos()
+        {            
+            for (int i = 0; i < torres.Count; i++)
+            {
+                int j = 0;
+                Stack<int> torreActual = new Stack<int>(torres[i]);
+                foreach (int discoSize in torreActual)
+                {
+                    int x = i * 325 + (190 - discos[discoSize - 1].Width) / 2;
+                    int y = 345 - (j + 1) * 20;
+                    discos[discoSize - 1].Location = new Point(x, y);
+                    Controls.Add(discos[discoSize - 1]);
+                    j++;
+                }
+            }
+            foreach (Button disco in discos)
+            {
+                disco.BringToFront();
+            }
+        }
+        private void MoverDisco(int origen, int destino)
+        {
+            int disco = torres[origen].Pop();
+            torres[destino].Push(disco);
+        }
+        private void ResolverHanoi(int n, int origen, int destino, int auxiliar)
+        {
+            if (n > 0)
+            {
+                ResolverHanoi(n - 1, origen, auxiliar, destino);
+                MoverDisco(origen, destino);
+                MostrarDiscos();
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(400); 
+                ResolverHanoi(n - 1, auxiliar, destino, origen);
+            }
+        }        
+        private void btnResolver_Click_1(object sender, EventArgs e)
+        {
+            if (torres == null || torres[TorreInicial].Count == 0)
+            {
+                MessageBox.Show("No hay torres o ya se ha resuelto.");
+            }            
+            else 
+            {
+                ResolverHanoi(numDiscos, TorreInicial, TorreFinal, TorreAuxiliar);
+                label1.Visible = true;
+            }
+        }       
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            if (comboFinal.SelectedItem == null || comboInicio.SelectedItem == null || comboNumDiscos.SelectedItem == null)
+            {
+                MessageBox.Show("Selección de torres o discos no válida");
+            }
+            else
+            {
+                if (int.TryParse(comboFinal.SelectedItem.ToString(), out TorreFinal) &&
+                    int.TryParse(comboInicio.SelectedItem.ToString(), out TorreInicial) &&
+                    int.TryParse(comboNumDiscos.SelectedItem.ToString(), out numDiscos))
+                {
+                    if (TorreFinal < 1 || TorreFinal > 3 || TorreInicial < 1 || TorreInicial > 3 || TorreFinal == TorreInicial)
+                    {
+                        MessageBox.Show("Selección de torres no válida");
+                        return;
+                    }
+                    if (numDiscos < 3 || numDiscos > 5 )
+                    {
+                        MessageBox.Show("Cantidad de discos no válida. Debe ser entre 3 y 5.");
+                        return;
+                    }
+                    TorreFinal--;
+                    TorreInicial--;
+                    TorreAuxiliar = Enumerable.Range(0, 3).Except(new[] { TorreInicial, TorreFinal }).Single();
+                    if (torres == null)
+                    {
+                        IniciarTorres();
+                        IniciarDiscos();                        
+                        MostrarDiscos();                        
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ya hay torre");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("La conversión no es válida. Asegúrate de que los elementos en los ComboBox sean números.");
+                }
+            }
+        }
+        private void LimpiarPantalla()
+        {
+            foreach (var disco in discos)
+            {
+                Controls.Remove(disco);
+            }
+            discos.Clear();
+            torres = null;
+            comboInicio.SelectedItem = null;
+            comboFinal.SelectedItem = null;
+            comboInicio.Text = inicioText; comboFinal.Text = finalText;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (torres == null)
+            {
+                MessageBox.Show("No hay nada para reiniciar.");
+            }
+            else
+            {
+                LimpiarPantalla();
+                label1.Visible = false;
+            }
+        }        
+        private void IniciarTorresVisuales()
+        {
+            torresVisuales = new List<Panel>();
+            for (int i = 0; i < 3; i++)
+            {
+                Panel torreVisual = new Panel();
+                torreVisual.BackColor = Color.Black;
+                torreVisual.Width = 10;torreVisual.Height = 120; 
+                int x = (i * 325) + 90;int y = 228; 
+                torreVisual.Location = new Point(x, y);
+                torresVisuales.Add(torreVisual);
+                Controls.Add(torreVisual);
+            }
+        }
+        private void MostrarTorresVisuales()
+        {
+            foreach (var torreVisual in torresVisuales)
+            {
+                torreVisual.BringToFront();
+            }
+        }
+        private void comboNumDiscos_SelectedIndexChanged(object sender, EventArgs e)
+        { }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        { }
+        private void comboFinal_SelectedIndexChanged(object sender, EventArgs e)
+        { }
+        private void Hanoi_Load(object sender, EventArgs e)
+        { }
+        private void btnResolver_Click(object sender, EventArgs e)
+        { }
+    }
+}
